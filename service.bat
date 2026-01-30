b809e81b0ec3e101b709494edca36c7c0253339a
.Punk <67797794+ArtworkPunk@users.noreply.github.com>
2026-01-30 16:09:22 +0300

    Update service.bat

--- service.bat
+++ service.bat
@@ -7,7 +7,7 @@ cd /d "%~dp0"
 
 :: --- CONFIGURATION ---
 set "APP_TITLE=ZAPRET TOOLKIT"
-set "LOCAL_VERSION=1.1.2 (ArtworkPunk Edition)"
+set "LOCAL_VERSION=1.2.0 (ArtworkPunk Edition)"
 set "REPO_URL=https://github.com/ArtworkPunk/Zapret-Toolkit"
 set "CHECKER_URL=https://hyperion-cs.github.io/dpi-checkers/ru/tcp-16-20/"
 set "HOSTS_FILE=%WINDIR%\System32\drivers\etc\hosts"
@@ -834,4 +834,5 @@ exit /b
 
 :PrintRes
 if "%2"=="1" ( echo  %c_grn%[+] %~1%c_rst% ) else if "%2"=="2" ( echo  %c_yel%[?] %~1%c_rst% ) else ( echo  %c_red%[X] %~1%c_rst% )
+
 exit /b
